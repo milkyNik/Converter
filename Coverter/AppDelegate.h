a5f2e9246b56e03f46b21ec74b71827c6201a0c7
milkyNik <milniklaus@gmail.com>
2016-04-15 14:02:46 +0300

    Initial Commit

---
+++ Coverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coverter
+//
+//  Created by iMac on 15.04.16.
+//  Copyright © 2016 MilNik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Coverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coverter
+//
+//  Created by iMac on 15.04.16.
+//  Copyright © 2016 MilNik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
